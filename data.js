4505efe008ef55966756e1a0d156356614d2ce38
E-RKP <edobackup111@gmail.com>
2025-05-09 14:02:12 +0200

    version 3

--- data.js
+++ data.js
@@ -1,2 +1,19 @@
 let ciao = 'ciao'
 console.log(ciao)
+const myButton = document.querySelector('button')
+const myTitle = document.querySelector('h1')
+const input = document.querySelector('input')
+
+function saluto (nome){
+
+  console.log(`ciao ${nome}`)
+  myTitle.innerHTML = `Ciao ${input.value}`;
+}
+
+
+
+myButton.addEventListener('click', ()=>{
+  saluto('spuff');
+  console.log(input.value)
+})
+
